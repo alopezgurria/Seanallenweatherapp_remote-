78b408ead919c7e34c9348530ceac7b1b3bb9778
Alexandra Lopezgurria <a.lopezgurria@gmail.com>
2023-10-25 17:57:55 +0200

    Initial Commit

---
+++ Seanallenweatherapp/SeanallenweatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeanallenweatherappApp.swift
+//  Seanallenweatherapp
+//
+//  Created by Alexandra LOPEZ-GURRIA on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SeanallenweatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
